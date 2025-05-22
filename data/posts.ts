376c103dc6300a7feaba34e2090a721ff8b91441
Dalila Castro <dalilaccastro@gmail.com>
2025-05-22 12:23:48 -0300

    Create posts.ts for testing with page.tsx

---
+++ data/posts.ts
@@ -0,0 +1,13 @@
+// data/posts.ts
+export const posts = [
+  {
+    slug: 'primeiro-post',
+    title: 'Meu Primeiro Post',
+    content: 'Este é o conteúdo do primeiro post.',
+  },
+  {
+    slug: 'segundo-post',
+    title: 'Testando o post 2',
+    content: 'Mais conteúdo do segundo post para aparecer aqui.',
+  },
+]
